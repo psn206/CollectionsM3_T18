8eb2cce3dd0671bca023de9a9efc9eb3e9880fb1
Sergei Prutsakov <psn206@mail.ru>
2024-05-03 13:45:55 +0300

    Добавьте файлы проекта.

---
+++ Task/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку со скобками:");
+            bool chek = Chek(Console.ReadLine());
+            Console.WriteLine(chek);
+            Console.ReadKey();
+        }
+
+        static bool Chek(string str)
+        {
+            Stack<char> stack = new Stack<char>();
+            Dictionary<char, char> dic = new Dictionary<char, char>();
+            dic.Add('(', ')');
+            dic.Add('{', '}');
+            dic.Add('[', ']');
+
+            foreach (char c in str)
+            {
+                if (c == '(' || c == '{' || c == '[')
+                {
+                    stack.Push(dic[c]);
+                }
+                if (c == ')' || c == '}' || c == ']')
+                {
+                    if (stack.Count == 0 || stack.Pop() != c)
+                    {
+                        return false;
+                    }
+                }
+            }
+            if (stack.Count == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
+
